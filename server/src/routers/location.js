eb2727a634c6c1152affb23eb9f119de8d717f04
13lav <lavsaini013@gmail.com>
2021-05-15 16:53:14 +0530

    Front End ready to call server

--- server/src/routers/location.js
+++ server/src/routers/location.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import {
-    getLocationAll,
-    getLocationByState,
+    //getLocationAll,
+    //getLocationByState,
     getLocationByDistrict,
 } from '../controllers/location.js';
 
 const router = express.Router();
 
 router
-    .route('/')
-    .get(getLocationAll)
-
-router
     .route('/:state_id/:district_id')
     .get(getLocationByDistrict)
-
+/*
 router
     .route('/:state_id')
     .get(getLocationByState)
 
+router
+    .route('/')
+    .get(getLocationAll)
+*/
 export default router;
